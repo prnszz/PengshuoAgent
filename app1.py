baee4b5fad2999b8c85596bba87f60509b5439e5
Pengshuo Qiu <149804000+prnszz@users.noreply.github.com>
2024-10-23 20:50:02 +0900

    requirement

--- app1.py
+++ app1.py
@@ -12,6 +12,10 @@ from dotenv import load_dotenv
 import chromadb
 import os
 import glob
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 
 # 设置页面配置
 st.set_page_config(page_title="Chat with Pengshuo", layout="wide")
